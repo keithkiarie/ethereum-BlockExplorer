f45914663392f30d5c37867b6223346b3be98046
Keithkiarie <keithkiarie@gmail.com>
2019-10-15 15:16:59 +0300

    for serving the html

---
+++ server.js
@@ -0,0 +1,22 @@
+var http = require('http'),
+    fs = require('fs');
+
+
+http.createServer(function (request, response) {
+
+    fs.readFile('./index.html', function (err, html) {
+        if (err) {
+            console.log(err);
+        }
+
+        response.writeHeader(200, {
+            "Content-Type": "text/html"
+        });
+        response.write(html);
+
+
+        response.end();
+    });
+
+}).listen(8080);
+console.log("Server started on port: 8080");
